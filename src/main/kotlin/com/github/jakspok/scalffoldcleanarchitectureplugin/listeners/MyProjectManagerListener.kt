f4cfc9aedaede5066b710bef3895416e1ef42778
GitHub Action <action@github.com>
2021-12-09 15:25:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/jakspok/scalffoldcleanarchitectureplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/jakspok/scalffoldcleanarchitectureplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.jakspok.scalffoldcleanarchitectureplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.jakspok.scalffoldcleanarchitectureplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/jakspok/scalffoldcleanarchitectureplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jakspok.scalffoldcleanarchitectureplugin.services
+
+import com.github.jakspok.scalffoldcleanarchitectureplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
